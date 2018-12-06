84ca3b017ee877841523369d626f123a48d8195a
grich89 <richg@merrimack.edu>
2018-12-06 09:30:43 -0500

    push up the build folder

---
+++ build/precache-manifest.f5c85bf1b230830c9248749df98fa34e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3724a3e2b109200f4f63",
+    "url": "/static/js/main.3724a3e2.chunk.js"
+  },
+  {
+    "revision": "bee0877370182e0eec7c",
+    "url": "/static/js/1.bee08773.chunk.js"
+  },
+  {
+    "revision": "3724a3e2b109200f4f63",
+    "url": "/static/css/main.dcb8a16e.chunk.css"
+  },
+  {
+    "revision": "358f8df1a72b4272b2db31f95ee9ad51",
+    "url": "/index.html"
+  }
+];
