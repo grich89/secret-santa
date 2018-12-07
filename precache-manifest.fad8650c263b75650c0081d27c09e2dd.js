abe3199945d24fdb03ae47155fe490cd73eb09a0
grich89 <richg@merrimack.edu>
2018-12-07 15:08:50 -0500

    Updates

--- precache-manifest.fad8650c263b75650c0081d27c09e2dd.js
+++ precache-manifest.fad8650c263b75650c0081d27c09e2dd.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/secret-santa/static/js/runtime~main.8532a1fc.js"
   },
   {
-    "revision": "c6cd997f2e9e957c3d9a",
-    "url": "/secret-santa/static/js/main.c6cd997f.chunk.js"
+    "revision": "fee88338aeb6d079cf6c",
+    "url": "/secret-santa/static/js/main.fee88338.chunk.js"
   },
   {
     "revision": "71e42591ef50100f5a60",
     "url": "/secret-santa/static/js/1.71e42591.chunk.js"
   },
   {
-    "revision": "c6cd997f2e9e957c3d9a",
-    "url": "/secret-santa/static/css/main.b0c8d35d.chunk.css"
+    "revision": "fee88338aeb6d079cf6c",
+    "url": "/secret-santa/static/css/main.af2a2b82.chunk.css"
   },
   {
-    "revision": "bf0aa4049bc17b6d9e91c91d95bf95f2",
+    "revision": "e59d2814a6e636a5fa944e585fc62469",
     "url": "/secret-santa/index.html"
   }
 ];
